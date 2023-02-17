81b318a2c41d54274b27dad428eb57c46e749aec
yahi-ko <igncupcake2015@gmail.com>
2023-02-17 00:40:01 -0500

    Initial commit

---
+++ src/main/java/net/yamato/megamon/Megamon.java
@@ -0,0 +1,20 @@
+package net.yamato.megamon;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Megamon implements ModInitializer {
+	public static final String MOD_ID = "megamon";
+	public static final Logger LOGGER = LoggerFactory.getLogger("megamon");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/yamato/megamon/MegamonClient.java
@@ -0,0 +1,10 @@
+package net.yamato.megamon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MegamonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
