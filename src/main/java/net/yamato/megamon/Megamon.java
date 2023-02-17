a88897ff44400b259b64195b041ce990dedc8661
yahi-ko <igncupcake2015@gmail.com>
2023-02-17 01:17:52 -0500

    added bead and aramasa card

--- src/main/java/net/yamato/megamon/Megamon.java
+++ src/main/java/net/yamato/megamon/Megamon.java
@@ -2,6 +2,7 @@ package net.yamato.megamon;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.yamato.megamon.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +12,6 @@ public class Megamon implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/yamato/megamon/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.yamato.megamon.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.yamato.megamon.Megamon;
+
+public class ModItems {
+    public static final Item BEAD = registerItem("bead",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item ARAMASA_CARD = registerItem("aramasa_card",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(Megamon.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Megamon.LOGGER.debug("Registering Mod Items for " + Megamon.MOD_ID);
+    }
+
+}
